e01f62f372f91ab103ed77e5d79746d9e7da038c
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2022-02-22 16:01:27 -0300

    feat: theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,23 @@
 interface ITheme {
   colors: {
     background: string;
+    red: string;
+    blue: string;
+    blueLight: string;
+    textTitle: string;
+    textBody: string;
+    shape: string;
   };
 }
 
 export const theme: ITheme = {
   colors: {
     background: "#F0F2F5",
+    red: "#E52E4D",
+    blue: "#5429CC",
+    blueLight: "#6933FF",
+    textTitle: "#363F5F",
+    textBody: "#969CB3",
+    shape: "#FFFFFF",
   },
 };
